7662c6f661ba5658c779ca98303b800f54c9d171
jacobmahoney <mahoney.jacob@gmail.com>
2017-11-16 01:52:42 -0600

    Added popup youtube video for the trailer.

--- website/movie-preview/main.js
+++ website/movie-preview/main.js
@@ -4,41 +4,36 @@ $(window).on("load", function() {
         popupDropdownShow(this);
     });
     
+    $("#movieInfoTrailerButton").on("click", function() {
+        showYoutubeVideo();
+    });
+    
+    $("#youtubeVideoX").on("click", function() {
+        hideYoutubeVideo();
+    });
+    
+    $(document).on("keyup", function(event) {
+        // user hits the escape key while viewing the trailer
+        if (event.which == 27) {
+            if ($("#youtubeVideoWrapper").css("display") == "flex" && $("#youtubeVideoWrapper").css("opacity") == "1") {
+                hideYoutubeVideo();
+            }
+        }
+    });
+    
     $(document).on("click", ".popupDropdownElement", function() {
         popupDropdownChange(this);
-    });
-    
-    $(document).on("click", ".popupDropdownElementStudent", function() {
-        
-        var dialogBox = $(this).closest(".dialogBox");
-        
-        if ($(dialogBox).attr("id") == "editStudentBox") {
-            editStudentUpdateFields(this);
-        }
-        
-        updateSiblingBookDropdown(this);
-        
-    });
-    
-    $(document).on("click", ".popupDropdownElementBook", function() {
-        
-        var dialogBox = $(this).closest(".dialogBox");
-        
-        if ($(dialogBox).attr("id") == "editBookBox") {
-            editBookUpdateFields(this);
-        }
-        
-        updateSiblingDataEntryDropdown(this);
-        
     });
     
     $(document).on("click", function(event) {
         
         // checking if any popup dropdowns need to be closed
-        var dropdowns = document.getElementsByClassName("popupDropdownElements");
+        var dropdowns = document.getElementsByClassName("popupDropdown");
+        var dropdownElements = document.getElementsByClassName("popupDropdownElements");
         for (var i = 0; i < dropdowns.length; i++) {
-            if ($(dropdowns[i]).css("height") != "0px") {
-                $(dropdowns[i]).animate({ height: "0" }, 200);
+            if ($(dropdowns[i]).attr("id") == "opened") {
+                $(dropdownElements[i]).animate({ height: "0" }, 200);
+                $(dropdowns[i]).attr("id", "closed");
             }
         }
         
@@ -48,16 +43,71 @@ $(window).on("load", function() {
 
 
 
+function showYoutubeVideo() {
+    
+    $("#youtubeVideoWrapper").css("display", "flex");
+    $("#youtubeVideoWrapper").css("opacity");
+    $("#youtubeVideoWrapper").css("opacity", "1");
+    
+    setTimeout(function() {
+        
+        $("#youtubeVideo").css("display", "block");
+        $("#youtubeVideo").css("top");
+        $("#youtubeVideo").css("top", "0");
+        $("#youtubeVideo").css("opacity");
+        $("#youtubeVideo").css("opacity", "1");
+        
+    }, 250);
+    
+}
+
+
+
+function hideYoutubeVideo() {
+    
+    $("#youtubeVideo").css("top");
+    $("#youtubeVideo").css("top", "-50px");
+    $("#youtubeVideo").css("opacity");
+    $("#youtubeVideo").css("opacity", "0");
+    
+    setTimeout(function() {
+        
+        setTimeout(function() {
+            $("#youtubeVideo").css("display", "none");
+        }, 100);
+        
+        $("#youtubeVideoWrapper").css("opacity");
+        $("#youtubeVideoWrapper").css("opacity", "0");
+        
+        setTimeout(function() {
+            $("#youtubeVideoWrapper").css("display", "none");
+        }, 350);
+        
+    }, 250);
+    
+    // refreshing (stopping) youtube video when it is hidden
+    var iframe = document.getElementById("youtubeVideo");
+    iframe.src = iframe.src;
+    
+}
+
+
+
 function popupDropdownShow(root) {
     
     if ($(root).parent().find(".popupDropdownElement").length > 0) { // only show dropdown if there are dropdown elements
+        
+        var popupDropdown = $(root).parent();
+        $(popupDropdown).attr("id", "opening");
         
         var dropdown = $(root).next();
         
         var h = getHeight(dropdown);
         if (h > 150) h = 150;
         
-        $(dropdown).animate({ height: h + "px" }, 200);
+        $(dropdown).animate({ height: h + "px" }, 200, function() {
+            $(popupDropdown).attr("id", "opened");
+        });
         
     }
     
@@ -68,45 +118,24 @@ function popupDropdownShow(root) {
 function popupDropdownChange(element) {
     
     var root = $(element).parent().parent().siblings()[0];
-    console.log(root);
+    var popupDropdown = $(root).parent();
+    
+    if ($(popupDropdown).hasClass("popupDropdownDays")) {
+        updateTimesDropdown($(element).html());
+    }
+    
     var rootText = $(root).children()[0];
     $(rootText).html($(element).html());
-    $(rootText).attr("id", $(element).attr("id"));
     
 }
 
 
 
-function updateSiblingBookDropdown(studentElement) {
+function updateTimesDropdown(day) {
     
-    var dialogBoxContent = $(studentElement).closest(".dialogBoxContent");
-    
-    var booksPopupDropdown = $(dialogBoxContent).find(".popupDropdownBooks")[0];
-    
-    if (booksPopupDropdown !== undefined) { // if there exists a book popup dropdown to update
-        
-        var studentsPopupDropdown = $(dialogBoxContent).find(".popupDropdownStudents")[0];
-        var studentsRootText = $(studentsPopupDropdown).find(".popupDropdownRootText")[0];
-        var student = getStudentByID($(studentsRootText).attr("id"));
-        
-        if (student != null) {
-            
-            resetPopupDropdown(dialogBoxContent, ".popupDropdownBooks", "select book");
-            resetPopupDropdown(dialogBoxContent, ".popupDropdownDataEntries", "select data entry");
-            
-            var booksDropdown = $(booksPopupDropdown).find(".popupDropdownElementsInner")[0];
-            var dropdownElement;
-            
-            for (var i = 0; i < student.books.length; i++) {
-                
-                dropdownElement = $("<div id=\"" + student.books[i].bookID + "\" class=\"popupDropdownElement popupDropdownElementBook\">" + student.books[i].name + "</div>");
-                dropdownElement.appendTo(booksDropdown);
-                
-            }
-            
-        }
-        
-    }
+    // function to update the times popup dropdown based on the day parameter
+    // so need to loop through all the times for a certain date to add the elements to the times popup dropdown
+    // not sure what else i can do here, i think this just requires some asp
     
 }
 
@@ -121,6 +150,11 @@ function getHeight(selector) {
     return h;
     
 }
+
+
+
+
+
 
 
 
