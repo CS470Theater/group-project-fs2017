972085fcd0eae65d0c50da08439bf21d7346194a
Skyinovinne <sxkfrvr@gmail.com>
2017-11-21 18:58:33 -0600

    Made preview page dynamic

--- TheaterBooking.Web/Areas/Movies/Controllers/PreviewController.cs
+++ TheaterBooking.Web/Areas/Movies/Controllers/PreviewController.cs
@@ -1,4 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TheaterBooking.Web.Areas.Admin.Models;
 
 namespace TheaterBooking.Web.Areas.Movies.Controllers
 {
@@ -7,14 +13,32 @@ namespace TheaterBooking.Web.Areas.Movies.Controllers
     /// </summary>
     public class PreviewController : Controller
     {
+        private readonly TheaterDBEntities _db;
+
+        /// <summary>
+        ///     Instantiates a new preview controller with the specified database model
+        /// </summary>
+        public PreviewController(TheaterDBEntities db)
+        {
+            _db = db;
+        }
+
         /// <summary>
         ///     Gets the home index page
         /// </summary>
         /// <returns>A view of the index page</returns>
         [Authorize(Roles = "web.home.view")]
-        public ActionResult Index()
+        public ActionResult Index(int movieId)
         {
-            return View();
+            var movie = _db.Movies.Where(m => m.Movie_ID == movieId)
+                .Include(m => m.Showtimes)
+                .SingleOrDefault();
+            if (movie == null)
+            {
+                throw new HttpException((int)HttpStatusCode.NotFound, "Movie not found");
+            }
+
+            return View(movie);
         }
     }
 }
