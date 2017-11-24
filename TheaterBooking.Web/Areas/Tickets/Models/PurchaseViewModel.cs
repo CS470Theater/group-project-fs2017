18bb501ef8b004261cb0d90f81b695d215a4ef3b
Skyinovinne <sxkfrvr@gmail.com>
2017-11-23 21:00:59 -0600

    Added ticket purchase controller

---
+++ TheaterBooking.Web/Areas/Tickets/Models/PurchaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TheaterBooking.Web.Areas.Tickets.Models
+{
+    public class PurchaseViewModel
+    {
+        public int? Quantity { get; set; }
+
+        public int ShowtimeId { get; set; }
+    }
+}
---
+++ TheaterBooking.Web/Areas/Tickets/TicketsAreaRegistration.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using JetBrains.Annotations;
+
+namespace TheaterBooking.Web.Areas.Tickets
+{
+    public class TicketsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName => "Tickets";
+
+        public override void RegisterArea([NotNull] AreaRegistrationContext context) 
+        {
+            context.MapRoute("Tickets_default", "Tickets/{controller}/{action}", new { action = "Index" });
+        }
+    }
+}
